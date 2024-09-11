03e34c60cd5c77c23e039145aec72e91b53c41b2
nathanielhershner <nehershner@crimson.ua.edu>
2024-09-10 19:59:30 -0500

    Initial page with full required functionality

---
+++ Fall2024-Assignment1-nehershner/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Fall2024_Assignment1_nehershner.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
